80c96e6a67e7cd4017826681f4c640528ce40386
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-11-29 10:46:29 +0000

    Imprement object state.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, {useState} from 'react'
 
 const App = (props) => {
-    // const initialStates = {
-    //     name: '',
-    //     price: 1000,
-    // }
-
-    const [name, setName] = useState(props.name)
-    const [price, setPrice] = useState(props.price)
-
-    const reset = () => {
-        setPrice(props.price)
-        setName(props.name)
-    }
+    const [state, setState] = useState(props)
+    const { name, price } = state
 
     return (
         <>
             <p>This is {name}'s {price}.</p>
-            <button onClick={()=> setPrice(price + 1)}>+1</button>
-            <button onClick={()=> setPrice(price - 1)}>-1</button>
-            <button onClick={reset}>Reset</button>
+            <button onClick={()=> setState({...state, price: price + 1})}>+1</button>
+            <button onClick={()=> setState({...state, price: price + 1})}>-1</button>
+            <button onClick={()=> {setState(props)}}>Reset</button>
 
-            <input value={name} onChange={e => setName(e.target.value)}/>
-            <button onClick={reset}>Reset</button>
-
-            {console.log(name)}
+            <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
         </>
     )
 }
