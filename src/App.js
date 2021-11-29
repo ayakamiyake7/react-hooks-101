7a83280ae70604cfbf0922efb0419957ccc162ea
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-11-29 11:45:17 +0000

    Imprement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState} from 'react'
 
 const App = (props) => {
     const [state, setState] = useState(props)
     const { name, price } = state
 
+    useEffect(() => {
+        console.log('This is like componentDidMount or componentDidUpdate.');
+    })
+    useEffect(() => {
+        console.log('This is like componentDidMount');
+    }, [])
+    useEffect(() => {
+        console.log('this callback is for name only.');
+    }, [name])
+
+    const renderPeriod = () => {
+        console.log('renderPeriod renders period');
+        return '。'
+    }
+
     return (
         <>
-            <p>This is {name}'s {price}.</p>
+            <p>This is {name}'s {price}.{renderPeriod()}</p>
             <button onClick={()=> setState({...state, price: price + 1})}>+1</button>
             <button onClick={()=> setState({...state, price: price + 1})}>-1</button>
             <button onClick={()=> {setState(props)}}>Reset</button>
