7813b397af2307001b45da3701cd59a526704e1b
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-11-29 20:39:39 +0000

    Refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,20 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Event from './Event'
+import EventForm from './EventForm'
+
+import Events from './Events'
 import reducer from '../reducers'
 
 const App = () => {
-    const [state, dispatch] = useReducer(reducer, [], )
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-
-    const addEvent = e => {
-        e.preventDefault()
-        //dispatch
-        dispatch({
-            type: 'CREATE_EVENT',
-            title,
-            body
-        })
-
-        setTitle('')
-        setBody('')
-    }
-
-    const deleteAllEvents = e => {
-        e.preventDefault()
-        const result = window.confirm('Are you sure you want to delete all events?')
-        if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
-    }
-
-    const unCreatable = title === '' || body === ''
+    const [state, dispatch] = useReducer(reducer, [] )
 
     return (
         <>
         <div className="container-fluid">
-            <h4>Form for Events</h4>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="formEventTitle">Title</label>
-                    <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="formEventBody">Body</label>
-                    <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
-                </div>
-                <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>Create an event</button>
-                <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>Delete all events</button>
-
-                <h4>Event Menu</h4>
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Body</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))
-                        }
-                    </tbody>
-                </table>
-            </form>
+            <EventForm state={state} dispatch={dispatch}/>
+            <Events state={state} dispatch={dispatch}/>
         </div>
         </>
     )
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,27 +1,25 @@
 import React from 'react';
 
 const Event = ({dispatch, event}) => {
-    {
-        const id = event.id
-            const handleClickDeleteButton = () => {
-                const result = window.confirm(`Are you sure you want to delete (id=${id})?`)
-                if(result) {dispatch({ type: 'DELETE_EVENT', id })}
-            }
+    const id = event.id
+        const handleClickDeleteButton = () => {
+            const result = window.confirm(`Are you sure you want to delete (id=${id})?`)
+            if(result) {dispatch({ type: 'DELETE_EVENT', id })}
+        }
 
-            return (
-            <tr>
-                <td>{id}</td>
-                <td>{event.title}</td>
-                <td>{event.body}</td>
-                <td>
-                    <button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>Delete</button>
-                </td>
-            </tr>
-            )
-        // state.map((event, index) => {
-            
-        // })
-    }
+        return (
+        <tr>
+            <td>{id}</td>
+            <td>{event.title}</td>
+            <td>{event.body}</td>
+            <td>
+                <button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>Delete</button>
+            </td>
+        </tr>
+        )
+    // state.map((event, index) => {
+        
+    // })
 }
 
 export default Event
