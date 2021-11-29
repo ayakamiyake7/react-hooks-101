f8030b1c984d9bfba1a55787d5c4be42babe4b61
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-11-29 12:06:59 +0000

    Modified template.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const App = () => {
+
+    return (
+        <>
+        <p>This is a template for React app.</p>
+        </>
+    )
+}
+
+export default App
